72a30ac203a6c21886cdc2f12720d93bc872fa5a
Jonathan Shales <jshales46@gmail.com>
2020-08-09 00:55:45 -0700

    day 2 part 2

--- 2016/src/aoc_2016_02.py
+++ 2016/src/aoc_2016_02.py
@@ -12,6 +12,19 @@ def parse_position_on_keypad(position: int) -> int:
     return keypad[first_index][second_index]
 
 
+def parse_position_on_keypad_part2(position: int) -> int:
+    keypad = [
+        [0, 0, 1, 0, 0],
+        [0, 2, 3, 4, 0],
+        [5, 6, 7, 8, 9],
+        [0, "A", "B", "C", 0],
+        [0, 0, "D", 0, 0],
+    ]
+    second_index = position % 10
+    first_index = (position - second_index) // 10
+    return keypad[first_index][second_index]
+
+
 def determine_numpad_position(current_position, instruction) -> int:
     if instruction == "L":
         current_position = max(current_position - 1, round(current_position / 10) * 10)
@@ -22,10 +35,30 @@ def determine_numpad_position(current_position, instruction) -> int:
     elif instruction == "U":
         current_position = max(current_position - 10, current_position % 10)
     else:
-        assert instruction != "D"
+        assert instruction == "D"
         current_position = min(current_position + 10, (current_position % 10) + 20)
     return current_position
 
+
+def move_if_possible(current_position: int, delta: int) -> int:
+    acceptable_keypad_position = [2, 11, 12, 13, 20, 21, 22, 23, 24, 31, 32, 33, 42]
+    if current_position + delta in acceptable_keypad_position:
+        return current_position + delta
+    else:
+        return current_position
+
+
+def determine_numpad_position_part2(current_position, instruction) -> int:
+    if instruction == "L":
+        delta = -1
+    elif instruction == "R":
+        delta = 1
+    elif instruction == "U":
+        delta = -10
+    else:
+        assert instruction == "D"
+        delta = 10
+    return move_if_possible(current_position, delta)
 
 
 def main():
@@ -37,6 +70,18 @@ def main():
             current_position = determine_numpad_position(current_position, instruction)
 
         print(parse_position_on_keypad(current_position))
+
+    # part 2
+    print("begin part 2")
+    current_position = 11
+    data = open(f"../input_data/input_{YEAR}_{DAY:02d}.txt", "r").readlines()
+    for line in data:
+        line = line.rstrip()
+        for instruction in line:
+            current_position = determine_numpad_position_part2(
+                current_position, instruction
+            )
+        print(parse_position_on_keypad_part2(current_position))
 
 
 if __name__ == "__main__":
