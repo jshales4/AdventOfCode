85b9a3f3a349442b2a5de8931a177730d8038992
Jonathan Shales <jshales46@gmail.com>
2020-08-09 01:10:57 -0700

    refactored

--- 2016/src/aoc_2016_02.py
+++ 2016/src/aoc_2016_02.py
@@ -1,54 +1,28 @@
 # AoC 2016 Day 2
+from typing import List, Union
 
 YEAR = 2016
 DAY = 2
 
 
-def parse_position_on_keypad(position: int) -> int:
-    keypad = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-    assert position in [0, 1, 2, 10, 11, 12, 20, 21, 22]
+def parse_position_on_keypad(position: int, keypad: List[List[Union[str, int]]]) -> int:
     second_index = position % 10
     first_index = (position - second_index) // 10
     return keypad[first_index][second_index]
 
 
-def parse_position_on_keypad_part2(position: int) -> int:
-    keypad = [
-        [0, 0, 1, 0, 0],
-        [0, 2, 3, 4, 0],
-        [5, 6, 7, 8, 9],
-        [0, "A", "B", "C", 0],
-        [0, 0, "D", 0, 0],
-    ]
-    second_index = position % 10
-    first_index = (position - second_index) // 10
-    return keypad[first_index][second_index]
-
-
-def determine_numpad_position(current_position, instruction) -> int:
-    if instruction == "L":
-        current_position = max(current_position - 1, round(current_position / 10) * 10)
-    elif instruction == "R":
-        current_position = min(
-            current_position + 1, round(current_position / 10) * 10 + 2
-        )
-    elif instruction == "U":
-        current_position = max(current_position - 10, current_position % 10)
-    else:
-        assert instruction == "D"
-        current_position = min(current_position + 10, (current_position % 10) + 20)
-    return current_position
-
-
-def move_if_possible(current_position: int, delta: int) -> int:
-    acceptable_keypad_position = [2, 11, 12, 13, 20, 21, 22, 23, 24, 31, 32, 33, 42]
-    if current_position + delta in acceptable_keypad_position:
+def move_if_possible(
+    current_position: int, delta: int, acceptable_keypad_positions
+) -> int:
+    if current_position + delta in acceptable_keypad_positions:
         return current_position + delta
     else:
         return current_position
 
 
-def determine_numpad_position_part2(current_position, instruction) -> int:
+def determine_numpad_position(
+    current_position, instruction, acceptable_positions
+) -> int:
     if instruction == "L":
         delta = -1
     elif instruction == "R":
@@ -58,30 +32,49 @@ def determine_numpad_position_part2(current_position, instruction) -> int:
     else:
         assert instruction == "D"
         delta = 10
-    return move_if_possible(current_position, delta)
+    return move_if_possible(current_position, delta, acceptable_positions)
 
 
 def main():
     current_position = 11
     data = open(f"../input_data/input_{YEAR}_{DAY:02d}.txt", "r").readlines()
+    code: List[int] = []
+    keypad = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
     for line in data:
         line = line.rstrip()
         for instruction in line:
-            current_position = determine_numpad_position(current_position, instruction)
+            current_position = determine_numpad_position(
+                current_position, instruction, [0, 1, 2, 10, 11, 12, 20, 21, 22]
+            )
 
-        print(parse_position_on_keypad(current_position))
+        code.append(parse_position_on_keypad(current_position, keypad))
+    print("Answer to part 1:", code)
 
     # part 2
     print("begin part 2")
     current_position = 11
-    data = open(f"../input_data/input_{YEAR}_{DAY:02d}.txt", "r").readlines()
+    code: List[Union[str, int]] = []
     for line in data:
         line = line.rstrip()
         for instruction in line:
-            current_position = determine_numpad_position_part2(
-                current_position, instruction
+            current_position = determine_numpad_position(
+                current_position,
+                instruction,
+                [2, 11, 12, 13, 20, 21, 22, 23, 24, 31, 32, 33, 42],
             )
-        print(parse_position_on_keypad_part2(current_position))
+        code.append(
+            parse_position_on_keypad(
+                current_position,
+                keypad=[
+                    [0, 0, 1, 0, 0],
+                    [0, 2, 3, 4, 0],
+                    [5, 6, 7, 8, 9],
+                    [0, "A", "B", "C", 0],
+                    [0, 0, "D", 0, 0],
+                ],
+            )
+        )
+    print("Answer to part 2:", code)
 
 
 if __name__ == "__main__":
